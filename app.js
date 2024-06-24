e2d52b0f026a92bde0c4b0602069c50a2902ce2a
Shawn M. Crowley <scrowley@buffalo.edu>
2024-06-24 14:08:44 -0400

    Initial Commit

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 // app.js
 const express = require('express');
+const { specs, swaggerUi } = require('./swagger');
 const app = express();
-const port = 9000;
+const port = 3001;
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 app.get('/', (req, res) => {
   res.send('Hello, Swagger!');
